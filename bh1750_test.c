183b8dd250964702a3f0c0f0c9f25e6ad2aca20e
Michal Cieslakiewicz <michal.cieslakiewicz@wp.pl>
2016-11-09 15:52:38 +0100

    bh1750_test updated. Timestamp added and resolution changed to High Mode2.
    Signed-off-by: Michal Cieslakiewicz <michal.cieslakiewicz@wp.pl>

--- bh1750_test.c
+++ bh1750_test.c
@@ -4,6 +4,8 @@
 #include <string.h>
 #include <unistd.h>
 
+#include <sys/time.h>
+
 #include <fcntl.h>		/* For O_* constants */
 #include <semaphore.h>
 
@@ -20,9 +22,10 @@ extern int optind, opterr, optopt;
 /* Show help */
 void help(char *progname)
 {
-	printf("Usage:\n\t%s [-s] [-c N]\n\n", progname);
+	printf("Usage:\n\t%s [-s] [-t] [-c N]\n\n", progname);
 	puts("Where:");
 	puts("\t-s\t - use system-wide user semaphore for I2C bus access (optional)");
+	puts("\t-t\t - include timestamp (optional)");
 	puts("\t-c N\t - run continously every N seconds (optional)");
 }
 
@@ -45,16 +48,20 @@ void i2cUnlock(int semflg, sem_t *sem)
 
 int main(int argc, char *argv[])
 {
-	int opt, semflg, cntsec;
+	int opt, semflg, tsflg, cntsec;
 	sem_t *i2csem;
+	struct timeval t;
 	double lx;
 
 	/* analyze command line */
 	semflg = 0;
+	tsflg = 0;
 	cntsec = 0;
-	while((opt = getopt(argc, argv, "sc:")) != -1) {
+	while((opt = getopt(argc, argv, "stc:")) != -1) {
 		if (opt == 's')
 			semflg = 1;
+		else if (opt == 't')
+			tsflg = 1;
 		else if (opt == 'c')
 			sscanf(optarg, "%d", &cntsec);
 		else if (opt == '?') {
@@ -85,13 +92,16 @@ int main(int argc, char *argv[])
 	/* initialize sensor */
 	BH1750_powerOn(fd);
 	BH1750_softReset(fd);
-	BH1750_setMode(fd, BH1750_MODE_CONT, BH1750_MODE_RES_H);
+	BH1750_setMode(fd, BH1750_MODE_CONT, BH1750_MODE_RES_H2);
 
 	/* read luminance */
 	for(;;)	{
+		gettimeofday(&t, NULL);
 		i2cLock(semflg, i2csem);
 		lx = BH1750_getLx(fd);
 		i2cUnlock(semflg, i2csem);
+		if (tsflg)
+			printf("%lu.%03u   ", t.tv_sec, t.tv_usec / 1000);
 		printf("L = %.3lf lx\n", lx);
 		if (cntsec)
 			sleep(cntsec);
